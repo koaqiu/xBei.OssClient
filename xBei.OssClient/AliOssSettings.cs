fd70386acda013bcae93f84595800e8857a46165
KoaQiu <koaqiu@gmail.com>
2023-10-15 17:03:45 +0800

    添加项目文件。

---
+++ xBei.OssClient/AliOssSettings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net.xBei.Clients.Oss {
+    /// <summary>
+    /// 阿里OSS设置
+    /// </summary>
+    public class AliOssSettings {
+        /// <summary>
+        /// 可以配置多个服务，key建议使用<c>BucketName</c>
+        /// </summary>
+        public Dictionary<string, AliOssSettings.Config> Services { get; set; } = default!;
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Config {
+            /// <summary>
+            /// 
+            /// </summary>
+            public string AccessKeyId { get; set; } = default!;
+            /// <summary>
+            /// 
+            /// </summary>
+            public string AccessKeySecret { get; set; } = default!;
+            /// <summary>
+            /// 
+            /// </summary>
+            public string EndPoint { get; set; } = default!;
+            /// <summary>
+            /// 
+            /// </summary>
+            public string BucketName { get; set; } = default!;
+            /// <summary>
+            /// 自定义域名（强烈建议设置）
+            /// </summary>
+            public string? ImageHost { get; set; }
+            /// <summary>
+            /// 是否默认（只能设置一个）
+            /// </summary>
+            public bool IsDefault { get; set; } = default;
+            //STS
+
+            /// <summary>
+            /// 
+            /// </summary>
+            public string BucketEndPoint { get; set; } = default!;
+            /// <summary>
+            /// 过期
+            /// </summary>
+            public string Expiration { get; set; } = default!;
+            /// <summary>
+            /// Token
+            /// </summary>
+            public string SecurityToken { get; set; } = default!;
+            /// <summary>
+            /// 授权路径
+            /// </summary>
+            public string Path { get; set; } = default!;
+        }
+    }
+}
